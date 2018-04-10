76054e3f5be88c9652f5e058bb4a80f538eeb5c0
David Anderton <dwanderton@gmail.com>
2018-04-10 13:37:35 -0400

    update html

--- app.py
+++ app.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 def homepage():
     return render_template('home.html', test=1)
 
+# TODO return a json of match particpants
+
+# TODO record results of a match 
+
+# TODO return the top results for an emotion
+
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
