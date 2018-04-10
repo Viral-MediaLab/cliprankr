969f347832697540533dd25e7cbe42180803944b
David Anderton <dwanderton@gmail.com>
2018-04-10 13:27:36 -0400

    update html

--- app.py
+++ app.py
@@ -4,47 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <!doctype html>
-    <html>
-    <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
-    </head>
-
-    <body>
-      <div class="container">
-        <div class="row">
-          <div class="col-sm">
-            <h1>Pick the Winner</h1>
-            <p>It is currently {time}.</p>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md">
-            <img src="https://picsum.photos/800/450" width="100%"/>
-            <button id="one" type="button" class="btn btn-primary mt-2 mb-4">This one</button>
-          </div>
-          <div class="col-md">
-            <img src="https://picsum.photos/800/450/?random" width="100%"/>
-            <button id="two" type="button" class="btn btn-primary mt-2 mb-4">That one</button>
-          </div>
-        </div>
-      </div>
-
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
-
-      <script>
-        $('button').click((e) => {console.log('POST about '+e.target.id);});
-      </script>
-    </body>
-    </html>
-    """.format(time=the_time)
+    return render_template('home.html', test=1)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
